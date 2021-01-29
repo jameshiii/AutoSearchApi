0c7efb6919561fab9870a162b3a842e86ac23b63
James B. Huffman <jbhuffman@pilotcat.com>
2021-01-29 14:08:15 -0600

    changed base vehicle class to an abstrct class

--- AutoSearchApi/ViewModels/Vehicle.cs
+++ AutoSearchApi/ViewModels/Vehicle.cs
@@ -2,14 +2,14 @@
 
 namespace AutoSearchApi.ViewModels
 {
-    public class Vehicle
+    public abstract class Vehicle
     {
         public int Id { get; set; }
         public decimal Price { get; set; }
         public string Model { get; set; }
         public int Mileage { get; set; }
         public string Color { get; set; }
-        public virtual string Status { get; }
+        public abstract string Status { get; }
         internal VehicleStatus VehicleStatus { get; set;}
     }
 }
